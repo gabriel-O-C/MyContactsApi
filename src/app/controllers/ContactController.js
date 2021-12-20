a0095e6cb40d2139801fd5e8b7640bb73a28341b
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-20 22:57:07 +0000

    feat: delete and show functions

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -6,8 +6,16 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
+    const { id } = request.params;
 
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    return response.json(contact);
   }
 
   store() {
@@ -18,8 +26,15 @@ class ContactController {
 
   }
 
-  delete() {
+  async delete(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
 
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    await ContactsRepository.delete(id);
+    return response.sendStatus(204);
   }
 }
 
