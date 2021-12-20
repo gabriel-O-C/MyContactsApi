a742d452c8865476095ff9e5dcfaca2e98194c97
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-20 14:55:59 +0000

    chore: eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send('hello world');
-})
+});
 
 app.listen(3000, () => console.log('Server is running at http://localhost:3000'));
