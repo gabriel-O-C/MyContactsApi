c88bfac69f7ec500bcd78a489ed21576d848a269
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2022-01-10 14:21:15 +0000

    feat: delete  method refactored

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,22 +1,4 @@
-const { v4 } = require('uuid');
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Kuroko',
-    email: 'kuroko@mail.com',
-    phone: 1232137912,
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'kagami',
-    email: 'kagami@mail.com',
-    phone: 1232137912,
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -35,11 +17,12 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+    `, [id]);
+    return deleteOp;
   }
 
   async create({
